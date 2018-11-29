96afdcb89961768603f940a8e9e25c9cbb0be0e8
Joaquin Astelarra <joaquinastelarra@gmail.com>
2018-11-29 13:44:24 -0300

    corrected images and spaces

--- config/website.js
+++ config/website.js
@@ -4,7 +4,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'Joaquin Astelarra', // Navigation and Site Title
-  siteTitleAlt: 'Jo', // Alternative Site title for SEO
+  siteTitleAlt: 'JA', // Alternative Site title for SEO
   siteUrl: 'https://joaquinastelarra.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
